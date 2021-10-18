9bd4519343570676a9c78540fb0c18cfa1e34b64
Martin Sisk <Brothermj@hotmail.com>
2021-10-18 18:42:38 -0500

    added Microsoft using statements in Payment and UsersController.cs file.

--- Dad-A-Store/Controllers/UsersController.cs
+++ Dad-A-Store/Controllers/UsersController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Dad_A_Store.DataAccess;
+using Dad_A_Store.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Dad_A_Store.DataAccess;
-using Dad_A_Store.Models;
 
 
 namespace Dad_A_Store.Controllers
