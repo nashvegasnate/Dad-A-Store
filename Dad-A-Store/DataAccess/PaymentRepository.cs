04c037f7f78f5feff1d020937c14d5f0796a54f6
Martin Sisk <Brothermj@hotmail.com>
2021-10-18 19:17:14 -0500

    added and reordered using statements for PaymentRepository.cs and UserRepository.cs files.

--- Dad-A-Store/DataAccess/PaymentRepository.cs
+++ Dad-A-Store/DataAccess/PaymentRepository.cs
@@ -1,12 +1,10 @@
 ﻿using Dad_A_Store.Models;
 using Dad_A_Store.DataAccess;
-Using
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
--- Dad-A-Store/DataAccess/UserRepository.cs
+++ Dad-A-Store/DataAccess/UserRepository.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Dad_A_Store.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Dad_A_Store.Models;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
