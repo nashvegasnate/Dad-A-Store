ac3a08f3ae6f7de8e87d77cd66149d033a232c1d
Caseywalker <casey.walker118@gmail.com>
2021-10-26 19:10:37 -0500

    Adding call to index for orders

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
 import Routes from '../helpers/Routes';
 import NavBar from '../components/NavBar';
+import getOrders from '../helpers/data/ordersData';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((authed) => {
@@ -16,6 +18,7 @@ function App() {
           uid: authed.uid
         };
         setUser(userInfoObj);
+        getOrders().then((ordersArray) => setOrders(ordersArray));
       } else if (user || user === null) {
         setUser(false);
       }
@@ -28,6 +31,7 @@ function App() {
         <NavBar user={user} />
         <Routes
         user={user}
+        orders={orders}
         />
       </Router>
     </div>
