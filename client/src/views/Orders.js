6c250345c27fb3dfba056232208d5a230a571bf3
Caseywalker <casey.walker118@gmail.com>
2021-10-26 19:50:50 -0500

    Creating view for all orders

--- client/src/views/Orders.js
+++ client/src/views/Orders.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import OrderCard from '../components/OrderCard';
 
 function Orders({ user, orders }) {
   return (
     <div>
-      <h2>This is Orders</h2>
+      <h2>This is Orders {user.uid}</h2>
+      {
+        orders.map((orderInfo) => (
+          <OrderCard
+          key={orderInfo.orderID}
+          orderID={orderInfo.orderID}
+          userID={orderInfo.userID}
+          paymentID={orderInfo.paymentID}
+          orderAmount={orderInfo.orderAmount}
+          orderDate={orderInfo.orderDate}
+          shipDate={orderInfo.shipDate}
+          />
+        ))
+      }
     </div>
   );
 }
 
 Orders.propTypes = {
-  
-}
-
+  user: PropTypes.any,
+  orders: PropTypes.array.isRequired
+};
 
 export default Orders;
