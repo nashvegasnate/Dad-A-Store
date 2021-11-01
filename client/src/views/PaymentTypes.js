5d6ae8e8483aa835810fcb540376e2138f6048fb
Martin Sisk <Brothermj@hotmail.com>
2021-11-01 16:05:24 -0500

    Inserted PaymentType key: values for Payments function for PaymentTypes.js file in the views folder.

--- client/src/views/PaymentTypes.js
+++ client/src/views/PaymentTypes.js
@@ -2,10 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PaymentTypeCard from '../components/PaymentCard';
 
-export default function Payments() {
+export default function Payments({user, payments}) {
   return (
     <div>
-      <h2> Payment Types </h2>
+      <h2> Payment Type info {user.userName} </h2>
+      {
+        payments.map((paymentInfo) => (
+          <PaymentTypeCard
+          Key={paymentInfo.itemID}
+          paymentID={paymentInfo.paymentID}
+          paymentType={paymentInfo.paymentType}
+          user={user}
+          />
+        ))
+      } 
     </div>
   );
 }
+
+Payments.propTypes = {
+    user: PropTypes.any,
+    payments: PropTypes.array.isRequired
+  };
