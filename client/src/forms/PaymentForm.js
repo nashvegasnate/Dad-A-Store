c5567875c2b26593211aa7c407390a4852637c86
Martin Sisk <Brothermj@hotmail.com>
2021-11-01 20:45:20 -0500

    imports for PaymentForm.js file.

--- client/src/forms/PaymentForm.js
+++ client/src/forms/PaymentForm.js
@@ -5,3 +5,26 @@ import {
   Button, Form, FormGroup, Input, Label
 } from 'reactstrap';
 import { addPaymentType, updatePaymentType } from '../helpers/data/paymentTypesData';
+
+const PaymentForm = ({
+    formTitle,
+    setPayments,
+    paymentType,
+    firebaseKey,
+  }) => {
+    const [payment, setPayments] = useState({
+      paymentType: paymentType || '',
+      firebaseKey: firebaseKey || null
+    });
+
+    const handleInputChange = (e) => {
+        setProject((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value,
+        }));
+};
+
+const history = useHistory();
+
+
+  }
