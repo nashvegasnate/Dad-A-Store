5bc3fad0855e859261810a6cabd2a5d3774ad20d
Caseywalker <casey.walker118@gmail.com>
2021-11-04 15:40:28 -0500

    Adding repository method to get cart details

---
+++ Dad-A-Store/DataAccess/CartDetailRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dad_A_Store.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dad_A_Store.DataAccess
+{
+  public class CartDetailRepository
+  {
+    readonly string _connectionString;
+
+    public CartDetailRepository(IConfiguration config)
+    {
+      _connectionString = config.GetConnectionString("TempDataAStore");
+    }
+
+    internal List<CartDetail> GetAll()
+    {
+      using var db = new SqlConnection(_connectionString);
+
+      var OrderDetails = db.Query<CartDetail>(@"SELECT *
+                                          FROM CARTDETAILS").ToList();
+
+      return OrderDetails;
+    }
+  }
+}
