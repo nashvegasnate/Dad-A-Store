1e3392b4a149869ed0f916b0bfbfae1ebf269ddd
Caseywalker <casey.walker118@gmail.com>
2021-11-04 21:16:50 -0500

    Adding repository method to update a cart

--- Dad-A-Store/DataAccess/CartRepository.cs
+++ Dad-A-Store/DataAccess/CartRepository.cs
@@ -74,5 +74,116 @@ namespace Dad_A_Store.DataAccess
 
     }
 
+    internal Cart AddToCart(Guid userID, NewOrder addedItem)
+    {
+      var db = new SqlConnection(_connectionString);
+
+      var userSql = @"SELECT *
+                    FROM USERS
+                    WHERE UserID = @userID";
+
+      var userOrdering = db.QueryFirstOrDefault<User>(userSql, new { userID });
+
+      var cartToUpdateSql = @"SELECT *
+                              FROM CARTS 
+                              WHERE UserID=@userID AND Completed=0";
+
+      var cartToUpdate = db.QueryFirstOrDefault<Cart>(cartToUpdateSql, new { userID });
+
+      var cartID = cartToUpdate.CartID;
+
+      var currentCartItemsSql = @"SELCT *
+                                  FROM CARTDETAILS
+                                  WHERE CartID=@cartID";
+
+      var currentCartDetails = db.Query<CartDetail>(currentCartItemsSql, new { cartID }).ToList();
+
+      var currentListOfItems = new List<NewOrder>();
+
+      foreach (var currentCart in currentCartDetails)
+      {
+        var thisItem = new NewOrder { ItemID = currentCart.ItemID, Quantity = currentCart.ItemQuantity };
+        currentListOfItems.Add(thisItem);
+      }
+
+      currentListOfItems.Add(addedItem);
+
+      var thisOrderSubtotal = 0m;
+
+      foreach (var cartItem in currentListOfItems)
+      {
+        var itemsQuery = @"SELECT *
+                         FROM ITEMS
+                         WHERE ItemID = @id";
+
+        var cartItemID = cartItem.ItemID;
+
+        var thisItem = db.QueryFirstOrDefault<Item>(itemsQuery, new { id = cartItemID });
+
+        var thisCartItemTotal = 0m;
+        thisCartItemTotal = Convert.ToDecimal(thisItem.ItemPrice * cartItem.Quantity);
+        thisOrderSubtotal += thisCartItemTotal;
+      }
+
+      var cartToReturn = new Cart
+      {
+        CartID = cartToUpdate.CartID,
+        UserID = cartToUpdate.UserID,
+        OrderAmount = thisOrderSubtotal,
+        Completed = false
+      };
+
+      var updateCartSql = @"UPDATE CARTS
+                            SET CartID = @CartID
+                                ,UserID = @UserID
+                                ,OrderAmount = @OrderAmount
+                                ,Completed = false
+                             WHERE CartID = @CartID";
+
+      var theUpdatedCart = db.Execute(updateCartSql, cartToReturn);
+
+      var theFinalCart = db.QueryFirstOrDefault<Cart>(cartToUpdateSql, new { userID });
+
+      foreach (var cartDetailItem in currentListOfItems)
+      {
+        var itemsQuery = @"SELECT *
+                         FROM ITEMS
+                         WHERE ItemID = @id";
+
+        var cartItemID = cartDetailItem.ItemID;
+
+        var thisItem = db.QueryFirstOrDefault<Item>(itemsQuery, new { id = cartItemID });
+
+        var createCartDetailToCreate = new CartDetail
+        {
+          CartID = theFinalCart.CartID,
+          ItemID = thisItem.ItemID,
+          ItemQuantity = cartDetailItem.Quantity,
+          ItemPrice = thisItem.ItemPrice,
+          Completed = false
+        };
+
+        var createCartDetailSql = @"IF NOT EXISTS(SELECT *
+                                                  FROM CARTDETAILS
+                                                  WHERE CartID=@CartID AND ItemID=@ItemID)
+                                                  INSERT INTO CARTS
+                                                  (CartID
+                                                  ,ItemID,
+                                                  ,ItemQuantity
+                                                  ,ItemPrice
+                                                  ,Completed)
+                                              VALUES
+                                                  (@CartID
+                                                  ,@ItemID
+                                                  ,ItemQuantity
+                                                  ,ItemPrice
+                                                  ,Completed)";
+
+        var resultOfAdd = db.Execute(createCartDetailSql, createCartDetailToCreate);
+      }
+
+      return theFinalCart;
+    }
+
   }
 }
