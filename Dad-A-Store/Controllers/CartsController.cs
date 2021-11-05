7074a5cc2c1c4923aa5a910d8d32ec311bde3eab
Caseywalker <casey.walker118@gmail.com>
2021-11-04 21:19:12 -0500

    Adding controller method to add item to cart

--- Dad-A-Store/Controllers/CartsController.cs
+++ Dad-A-Store/Controllers/CartsController.cs
@@ -44,6 +44,12 @@ namespace Dad_A_Store.Controllers
       return _repo.CreateNewCart(userID);
     }
 
+    [HttpPut("/updateCart/{userID}")]
+    public Cart addItem(Guid userID, NewOrder cartItem)
+    {
+      return _repo.AddToCart(userID, cartItem);
+    }
+
 
   }
 }
