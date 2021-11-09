8a184b57d075b786e9dd3db7d7cc39107502cfc6
Martin Sisk <Brothermj@hotmail.com>
2021-11-09 13:12:10 -0600

    reordered import tags in index.js file.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import firebase from 'firebase';
-import './styles/index.scss';
+import firebaseConfig from './helpers/apiKeys';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './styles/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import firebaseConfig from './helpers/apiKeys';
 
 firebase.initializeApp(firebaseConfig);
 
