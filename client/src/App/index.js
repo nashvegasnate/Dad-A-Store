3528c967b30183588033acf336e94921b0b2fa06
Martin Sisk <Brothermj@hotmail.com>
2021-11-09 13:21:14 -0600

    imported getCategories and set categoreis useState in index.js file.

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import firebase from 'firebase';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.scss';
 import Routes from '../helpers/Routes';
 import NavBar from '../components/NavBar';
 import getItems from '../helpers/data/itemsData';
 import getOrders from '../helpers/data/ordersData';
+import getCategories from '../helpers/data/categoriesData';
 import { getValidUser, getUserWithUID } from '../helpers/data/usersData';
+import './App.scss';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -14,6 +15,7 @@ function App() {
   const [registeredUser, setRegisteredUser] = useState(false);
   const [orders, setOrders] = useState([]);
   const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((authed) => {
@@ -26,6 +28,7 @@ function App() {
         getValidUser(authed.uid).then((validResp) => setRegisteredUser(validResp));
         getOrders().then((ordersArray) => setOrders(ordersArray));
         getItems().then((itemsArray) => setItems(itemsArray));
+        getCategories().then((categoriesArray) => setCategories(categoriesArray));
         setUser(userInfoObj);
       } else if (user || user === null) {
         setUser(false);
@@ -48,6 +51,7 @@ function App() {
         user={user}
         orders={orders}
         items={items}
+        categories={categories}
         registeredUser={registeredUser}
         userFromDB={userFromDB}
         />
