1d6b5091faaace8a2548b696891ce59e89903f96
Caseywalker <casey.walker118@gmail.com>
2021-11-10 19:07:18 -0600

    Adding item info to cart details

--- client/src/components/CartDetailCard.js
+++ client/src/components/CartDetailCard.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Card, CardText, CardBody,
   CardTitle
 } from 'reactstrap';
 import PropTypes from 'prop-types';
+import { getItemByItemID } from '../helpers/data/itemsData';
 
 function CartDetailCard({
   itemID,
   itemQuantity,
   itemPrice
 }) {
+  const [item, setItem] = useState({});
+
+  useEffect(() => {
+    getItemByItemID(itemID).then((itemObject) => setItem(itemObject));
+  }, []);
   return (
     <div>
       <Card className='expense-cards'>
         <CardBody>
-          <CardTitle tag="h3">{itemID}</CardTitle>
+          <CardTitle tag="h3">{item.itemName}</CardTitle>
+          <CardText>Description: {item.itemDescription}</CardText>
           <CardText>Item Price: ${itemPrice}</CardText>
           <CardText>Quantity: {itemQuantity}</CardText>
         </CardBody>
