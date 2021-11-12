67034071220a4793575692c784593fa94961f9d5
Caseywalker <casey.walker118@gmail.com>
2021-11-11 18:40:36 -0600

    Adding update cart form

---
+++ client/src/components/CartDetailForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
+
+function CartDetailForm({
+  userID,
+  itemID,
+  quantity
+}) {
+  const [cartDetail, setCartDetail] = useState({
+    itemID: itemID || '',
+    quantity: quantity || 1
+  });
+
+  const handleNumberInput = (e) => {
+    setCartDetail((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.valueAsNumber
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.warn(`edited it bruh ${userID}`);
+  };
+
+  return (
+    <div className='form-group'>
+      <form className='mt-3' id='add-expense-form' autoComplete='off' onSubmit={handleSubmit}>
+        <h2>Edit Cart Item</h2>
+        <label>Update Quantity:</label>
+        <input
+        className='ml-2'
+        name='quantity'
+        type='number'
+        placeholder='1'
+        value={cartDetail.quantity}
+        onChange={handleNumberInput} />
+        <br/>
+        <Button color='success' type='submit'>Submit</Button>
+      </form>
+    </div>
+  );
+}
+
+CartDetailForm.propTypes = {
+  userID: PropTypes.any.isRequired,
+  itemID: PropTypes.string.isRequired,
+  quantity: PropTypes.number.isRequired
+};
+
+export default CartDetailForm;
