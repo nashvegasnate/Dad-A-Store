2090f1d598ec2bffc37237522758b9a696f1d4ab
Caseywalker <casey.walker118@gmail.com>
2021-11-17 20:54:56 -0600

    Scaffolding orders page

--- client/src/views/MyOrders.js
+++ client/src/views/MyOrders.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {
+  Card, CardText, CardBody,
+  CardTitle, Button
+} from 'reactstrap';
 
 export default function MyOrders() {
   return (
     <div>
-      <h2>This is your orders page.</h2>
+      <Card className='income-cards'>
+        <CardBody>
+          <CardTitle tag="h3">This is your orders page</CardTitle>
+          <CardText>Choose an option</CardText>
+          <br />
+          <Button className='mt-1' color='info'>Open Orders</Button>
+          <Button className='ml-1 mt-1' color='info'>Completed Orders</Button>
+        </CardBody>
+      </Card>
     </div>
   );
 }
