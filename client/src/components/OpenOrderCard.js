30d641c3db91e2e1c5711446fcf2157f4e869a48
Caseywalker <casey.walker118@gmail.com>
2021-11-20 12:48:00 -0600

    Adding cards for open and complete orders

---
+++ client/src/components/OpenOrderCard.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Card, CardText, CardBody,
+  CardTitle
+} from 'reactstrap';
+import PropTypes from 'prop-types';
+import { getUserPaymentType } from '../helpers/data/paymentTypesData';
+
+function OpenOrderCard({
+  userFromDB,
+  orderID,
+  paymentID,
+  orderAmount,
+  orderDate
+}) {
+  const [userPayment, setUserPayment] = useState(null);
+
+  useEffect(() => {
+    getUserPaymentType(paymentID).then((paymentObj) => setUserPayment(paymentObj));
+  }, []);
+
+  return (
+    <div>
+      <Card className='expense-cards'>
+        <CardBody>
+          <CardTitle tag="h3">Order for {userFromDB.userFirst} {userFromDB.userLast}</CardTitle>
+          <CardText>Order Number: {orderID}</CardText>
+          <CardText>Order Amount: ${orderAmount}</CardText>
+          { userPayment && <CardText>Payment Type: {userPayment[0].paymentType}</CardText> }
+          <CardText>Order Date: {orderDate}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+OpenOrderCard.propTypes = {
+  userFromDB: PropTypes.any.isRequired,
+  orderID: PropTypes.string.isRequired,
+  paymentID: PropTypes.string.isRequired,
+  orderAmount: PropTypes.number.isRequired,
+  orderDate: PropTypes.any.isRequired
+};
+
+export default OpenOrderCard;
