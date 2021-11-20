53b6b050b2aac57f85c31f4c0cee9c5f80c4aefb
Martin Sisk <Brothermj@hotmail.com>
2021-11-20 13:21:04 -0600

    fixed merge conflicts and deleted category.js files.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -26,17 +26,10 @@ const NavBar = ({ user, registeredUser }) => {
         <Link className="nav-link" to="/items">Items</Link>
       </NavItem>
       <NavItem>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <Link className="nav-link" to="/categories">Categories</Link>
-=======
-=======
         <Link className="nav-link" to="/departments">Departments</Link>
       </NavItem>
       <NavItem>
->>>>>>> a21766d0983c7f1bd6b4234cc07e964f1bab87a2
         <Link className="nav-link" to="/paymenttypes">Payments</Link>
->>>>>>> e8855fdb5f70070d61968e8e6e189fc50b5dee3d
       </NavItem>
       <NavItem>
         <Link className="nav-link" to="/itemsForms">Items-Form</Link>
