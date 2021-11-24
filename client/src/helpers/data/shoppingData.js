59a0ee7c3b840befc804c461718b236843e2346f
Martin Sisk <Brothermj@hotmail.com>
2021-11-23 19:35:39 -0600

    commiting before doing a review of PR #47.

---
+++ client/src/helpers/data/shoppingData.js
@@ -0,0 +1,10 @@
+// import axios from 'axios';
+// import firebaseConfig from '../apiKeys';
+
+// const dbURL = firebaseConfig.databaseURL;
+
+// const getItemByItemID = (itemID) => new Promise((resolve, reject) => {
+//     axios.get(`${dbURL}/api/items/CGetItemByItemID/${itemID}`)
+//       .then((response) => resolve(response.data))
+//       .catch((error) => reject(error));
+//   });
