9a9175091c4da1d5a5e6f0dba175934ed200eef5
Martin Sisk <Brothermj@hotmail.com>
2021-11-29 18:19:13 -0600

    commiting before pulling down PR #47 and merging into mjs-shopping.

---
+++ client/src/forms/ShoppingForm.js
@@ -0,0 +1,29 @@
+// import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import getItemByName from '../helpers/data/shoppingData';
+// import ItemCard from '../components/ItemCard';
+
+// const SearchBarElement = styled.div`
+//   input[type=text] {
+//     margin-top: 25px;
+//     padding: 12px;
+//     font-size: 15px;
+//     border: 2px solid #e7e7e7;
+//     background-color: #f3f3f3;
+//     align-content: center;
+//     width: 50%;
+//     border-radius: 25px;
+//     border-color: #7f7f7f;
+//   }
+//   `;
+
+// const SearchBar = () => {
+//   const [searchItem, setSearchItem] = useState([]);
+// };
+
+// const handleInputChange = (e) => {
+//   setSearchItem((prevState) => ({
+//     ...prevState,
+//     [e.target.name]: e.target.value,
+//   }));
+// };
