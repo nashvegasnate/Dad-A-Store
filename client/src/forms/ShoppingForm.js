a97daade18f104974928de28b7fa18e452ab4049
Martin Sisk <Brothermj@hotmail.com>
2021-12-01 16:14:15 -0600

    fixed all EsLint spacing errors in ShoppingForm.js file.

--- client/src/forms/ShoppingForm.js
+++ client/src/forms/ShoppingForm.js
@@ -33,55 +33,54 @@ export default function SearchBar() {
   const handleSearch = (e) => {
     e.preventDefault();
     setSearchItem([]);
-    // getItemByName(e.target.value)
-    getItemByName(searchItem).then((Response) => {
+    getItemByName(e.target.value).then((Response) => {
       setSearchItem(Response);
     });
   };
 
   return (
-    <div className='search-form'>
-        <SearchBarElement>
-    <Form
-      id='SearchForm'
-      autoComplete='off'
-      onSubmit={handleSearch}
-    >
-        <Label for="Search Items">Search Items</Label>
-        <Input
-            name='SearchInput'
-            id='SearchInput'
-            // defaultValue={categoryDescription}
-            type='text'
-            placeholder='Search'
-            onChange={handleInputChange}
-          />
-          <Button type='submit'>Search</Button>
-    </Form>
-    </SearchBarElement>
-    <div className='item-container'>
-    {
-        searchItem.length > 0
-          ? searchItem.map((item, itemID) => (
-        <ItemCard
-        key={itemID}
-        itemID={item[0].itemID}
-        itemName={item[0].itemName}
-        itemDescription={item[0].itemDescription}
-        itemPrice={item[0].itemPrice}
-        categoryID={item[0].categoryID}
-        sellerID={item[0].sellerID}
-        categoryName={item[0].categoryName}
-        sellerFirstName={item[0].sellerFirstName}
-        sellerLastName={item[0].sellerLastName}
-        user={user}
-        setItems={setItems}
-        userFromDB={userFromDB}
-        />
-          ))
-          : ''
-      }
-    </div>
-    </div>
+        <div className='search-form'>
+            <SearchBarElement>
+                <Form
+                    id='SearchForm'
+                    autoComplete='off'
+                    onSubmit={handleSearch}
+                >
+                    <Label for="Search Items">Search Items</Label>
+                    <Input
+                        name='SearchInput'
+                        id='SearchInput'
+                        // defaultValue={categoryDescription}
+                        type='text'
+                        placeholder='Search'
+                        onChange={handleInputChange}
+                    />
+                    <Button type='submit'>Search</Button>
+                </Form>
+            </SearchBarElement>
+            <div className='item-container'>
+                {
+            searchItem.length > 0
+              ? searchItem.map((item, itemID) => (
+                    <ItemCard
+                        key={itemID}
+                        itemID={item[0].itemID}
+                        itemName={item[0].itemName}
+                        itemDescription={item[0].itemDescription}
+                        itemPrice={item[0].itemPrice}
+                        categoryID={item[0].categoryID}
+                        sellerID={item[0].sellerID}
+                        categoryName={item[0].categoryName}
+                        sellerFirstName={item[0].sellerFirstName}
+                        sellerLastName={item[0].sellerLastName}
+                        // user={user}
+                        // setItems={setItems}
+                        // userFromDB={userFromDB}
+                        />
+              ))
+              : ''
+                }
+            </div>
+        </div>
   );
 }
