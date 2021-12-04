6b81bcc66d4fb1f72d674efb5655cef31bbaf292
Martin Sisk <Brothermj@hotmail.com>
2021-12-03 21:42:58 -0600

    deleted .cache folder.

--- client/src/forms/ShoppingForm.js
+++ client/src/forms/ShoppingForm.js
@@ -1,71 +1,78 @@
 import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import {
-  Button, Form, Input, Label
+    Button, Form, Input, Label
 } from 'reactstrap';
 import { getItemByName } from '../helpers/data/itemsData';
 import ItemCard from '../components/ItemCard';
 
-export default function SearchBar() {
-  const [searchItem, setSearchItem] = useState('');
-  const [resultSearch, setResultSearch] = useState([]);
+export default function SearchBar({
+    user, setItems, userFromDB }) {
+    const [searchItem, setSearchItem] = useState('');
+    const [resultSearch, setResultSearch] = useState([]);
 
-  const handleInputChange = (e) => {
-    setSearchItem((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
+    const handleInputChange = (e) => {
+        setSearchItem((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    // setSearchItem([]);
-    getItemByName(searchItem.SearchInput).then((Response) => {
-      setResultSearch(Response);
-    });
-  };
+    const handleSearch = (e) => {
+        e.preventDefault();
+        getItemByName(searchItem.SearchInput).then((Response) => {
+            setResultSearch(Response);
+        });
+    };
 
-  console.warn(resultSearch);
+    console.warn(resultSearch);
 
-  return (
+    return (
         <div className='search-form'>
-                <Form
-                    id='SearchForm'
-                    autoComplete='off'
-                    onSubmit={handleSearch}
-                >
-                    <Label for="Search Items">Search Items</Label>
-                    <Input
-                        name='SearchInput'
-                        id='SearchInput'
-                        // value={item.itemName}
-                        type='text'
-                        placeholder='Search'
-                        onChange={handleInputChange}
-                    />
-                    <Button type='submit'>Search</Button>
-                </Form>
+            <Form
+                id='SearchForm'
+                autoComplete='off'
+                onSubmit={handleSearch}
+            >
+                <Label for="Search Items">Search Items</Label>
+                <Input
+                    name='SearchInput'
+                    id='SearchInput'
+                    // value={item.itemName}
+                    type='text'
+                    placeholder='Search'
+                    onChange={handleInputChange}
+                />
+                <Button type='submit'>Search</Button>
+            </Form>
             <div className='item-container'>
                 {
-            resultSearch.length > 0
-              ? <ItemCard
-                        key={resultSearch[0].itemID}
-                        itemID={resultSearch[0].itemID}
-                        itemName={resultSearch[0].itemName}
-                        itemDescription={resultSearch[0].itemDescription}
-                        itemPrice={resultSearch[0].itemPrice}
-                        categoryID={resultSearch[0].categoryID}
-                        sellerID={resultSearch[0].sellerID}
-                        categoryName={resultSearch[0].categoryName}
-                        sellerFirstName={resultSearch[0].sellerFirstName}
-                        sellerLastName={resultSearch[0].sellerLastName}
-                        // user={user}
-                        // setItems={setItems}
-                        // userFromDB={userFromDB}
+                    resultSearch.length > 0
+                        ? <ItemCard
+                            key={resultSearch[0].itemID}
+                            itemID={resultSearch[0].itemID}
+                            itemName={resultSearch[0].itemName}
+                            itemDescription={resultSearch[0].itemDescription}
+                            itemPrice={resultSearch[0].itemPrice}
+                            categoryID={resultSearch[0].categoryID}
+                            sellerID={resultSearch[0].sellerID}
+                            categoryName={resultSearch[0].categoryName}
+                            sellerFirstName={resultSearch[0].sellerFirstName}
+                            sellerLastName={resultSearch[0].sellerLastName}
+                            user={user}
+                            setItems={setItems}
+                            userFromDB={userFromDB}
                         />
-              : ''
+                        : ''
                 }
             </div>
         </div>
-  );
+    );
 }
+
+SearchBar.propTypes = {
+    user: PropTypes.any.isRequired,
+    item: PropTypes.array,
+    setItems: PropTypes.any,
+    userFromDB: PropTypes.any.isRequired
+};
